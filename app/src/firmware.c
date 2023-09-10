265f3312d21799c46bbf1a7668adaadca48d2709
Loh Siu Yin <siuyin@beyondbroadcast.com>
2023-09-10 12:39:29 +0800

    refactor: create shared code folder

--- app/src/firmware.c
+++ app/src/firmware.c
@@ -2,7 +2,7 @@
 #include <libopencm3/stm32/gpio.h>
 #include <libopencm3/cm3/scb.h>
 #include "core/system.h"
-#include "core/timer.h"
+#include "timer.h"
 
 #define LED_PORT (GPIOA)
 #define LED_PIN (GPIO5)
--- bootloader/src/bootloader.c
+++ bootloader/src/bootloader.c
@@ -1,15 +1,21 @@
 #include "common-defines.h"
 #include <libopencm3/stm32/memorymap.h>
+#include <libopencm3/cm3/vector.h>
 
 #define BOOTLOADER_SIZE (0X8000U)
 #define MAIN_APP_START_ADDR (FLASH_BASE + BOOTLOADER_SIZE)
 
 static void jump_to_main(void) {
+	/*
 	typedef void (*void_fn)(void);
-	uint32_t* reset_vector_entry = (uint32_t*)(MAIN_APP_START_ADDR + 4U);
+	uint32_t* reset_vector_entry = (uint32_t*)MAIN_APP_START_ADDR + 1;
 	uint32_t* reset_vector = (uint32_t*)(*reset_vector_entry);
 	void_fn jump_fn = (void_fn)reset_vector;
 	jump_fn();
+	*/
+
+	vector_table_t* main_vector_table = (vector_table_t*)MAIN_APP_START_ADDR;
+	main_vector_table->reset();
 }
 
 int main(void) {
