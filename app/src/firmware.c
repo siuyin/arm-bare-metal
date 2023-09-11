dfb78ce351892ba523fc85b649955950c8c6c61a
Loh Siu Yin <siuyin@beyondbroadcast.com>
2023-09-11 12:48:44 +0800

    fix: revert

--- app/src/firmware.c
+++ app/src/firmware.c
@@ -2,10 +2,15 @@
 #include <libopencm3/stm32/gpio.h>
 #include <libopencm3/cm3/scb.h>
 #include "core/system.h"
+#include "core/uart.h"
 #include "timer.h"
 
 #define LED_PORT (GPIOA)
 #define LED_PIN (GPIO5)
+
+#define UART_PORT (GPIOA)
+#define RX_PIN (GPIO3)
+#define TX_PIN (GPIO2)
 
 #define BOOTLOADER_SIZE (0x8000U)
 static void vector_setup(void) {
@@ -16,6 +21,9 @@ static void gpio_setup(void) {
 	rcc_periph_clock_enable(RCC_GPIOA);
 	gpio_mode_setup(LED_PORT, GPIO_MODE_AF, GPIO_PUPD_NONE, LED_PIN);
 	gpio_set_af(LED_PORT, GPIO_AF1, LED_PIN);
+
+	gpio_mode_setup(UART_PORT, GPIO_MODE_AF, GPIO_PUPD_NONE, RX_PIN|TX_PIN);
+	gpio_set_af(UART_PORT, GPIO_AF7, RX_PIN|TX_PIN);
 }
 
 int main(void) {
@@ -23,6 +31,7 @@ int main(void) {
 	system_setup();
 	gpio_setup();
 	timer_setup();
+	uart_setup();
 
 	uint64_t start_time = system_get_ticks();
 	float duty_cycle = 0.0f;
@@ -35,6 +44,11 @@ int main(void) {
 			}
 			timer_pwm_set_duty_cycle(duty_cycle);
 			start_time = system_get_ticks();
+		}
+
+		if (uart_data_available()) {
+			uint8_t data = uart_read_byte();
+			uart_write_byte(data+1);
 		}
 	}
 
