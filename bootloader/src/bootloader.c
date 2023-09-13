fd0a52388c66fc05c7a2681be1913a816f1ff794
Loh Siu Yin <siuyin@beyondbroadcast.com>
2023-09-13 11:10:10 +0800

    feat: implement packet comms in bootloader

--- bootloader/src/bootloader.c
+++ bootloader/src/bootloader.c
@@ -1,9 +1,26 @@
 #include "common-defines.h"
 #include <libopencm3/stm32/memorymap.h>
 #include <libopencm3/cm3/vector.h>
+#include <libopencm3/stm32/rcc.h>
+#include <libopencm3/stm32/gpio.h>
+
+#include "core/uart.h"
+#include "core/system.h"
+#include "comms.h"
 
 #define BOOTLOADER_SIZE (0X8000U)
 #define MAIN_APP_START_ADDR (FLASH_BASE + BOOTLOADER_SIZE)
+
+#define UART_PORT (GPIOA)
+#define RX_PIN (GPIO3)
+#define TX_PIN (GPIO2)
+
+static void gpio_setup(void) {
+	rcc_periph_clock_enable(RCC_GPIOA);
+
+	gpio_mode_setup(UART_PORT, GPIO_MODE_AF, GPIO_PUPD_NONE, RX_PIN|TX_PIN);
+	gpio_set_af(UART_PORT, GPIO_AF7, RX_PIN|TX_PIN);
+}
 
 static void jump_to_main(void) {
 	/*
@@ -19,6 +36,32 @@ static void jump_to_main(void) {
 }
 
 int main(void) {
+	system_setup();
+	gpio_setup();
+	uart_setup();
+	comms_setup();
+
+	comms_packet_t pkt = {
+		.length = 9,
+		.data = {1,2,3,4,5,6,7,8,9,0xff,0xff,0xff,0xff,0xff,0xff,0xff},
+		.crc = 0
+	};
+	pkt.crc = comms_compute_crc(&pkt);
+
+	while (true) {
+		comms_update();
+
+		comms_packet_t rx_pkt;
+		if (comms_packets_available()) {
+			comms_read(&rx_pkt);
+		}
+
+		comms_write(&pkt);
+		system_delay(500);
+	}
+
+	// TODO: teardown, de-initialized peripherals.
+
 	jump_to_main();
 	return 0;
 }
